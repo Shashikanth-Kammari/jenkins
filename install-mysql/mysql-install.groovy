653b382d88c588cc53b5f7b8a02dc83a039bd576
Shashi <shashikanth0312@gmail.com>
2026-02-25 11:37:17 +0530

    adding

--- install-mysql/mysql-install.groovy
+++ install-mysql/mysql-install.groovy
@@ -1,5 +1,22 @@
 pipeline {
-    agent any
+    agent {
+        label 'my-defined-label'
+    }
+    parameters {
+        string(name: 'PERSON', defaultValue: 'Mr Shashi', description: 'This is your first jenkins job ?')
+
+        text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+
+        booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
+
+        choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+
+        password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+    }
+    options {
+                // Timeout counter starts BEFORE agent is allocated
+                timeout(time:  10, unit: 'SECONDS')
+            }
     environment {
         EC2_IP ="12.110.0.10" 
     }
@@ -16,7 +33,19 @@ pipeline {
                     sudo systemctl status mysqld
                 EOF
                 """
+                sleep(5)
                 }
+            steps{
+                echo "Hello ${params.PERSON}"
+
+                echo "Biography: ${params.BIOGRAPHY}"
+
+                echo "Toggle: ${params.TOGGLE}"
+
+                echo "Choice: ${params.CHOICE}"
+
+                echo "Password: ${params.PASSWORD}"
+            }
             }
         }
     }
